9db93a010bd32f025a79d078274d3116a0170a93
Tony Liang <b04901019@ntu.edu.tw>
2019-01-12 18:03:26 +0800

    update homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -7,9 +7,7 @@ import { ContractService } from '../core/services/contract.service';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent implements OnInit {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   constructor(
-    private contractService: ContractService
   ) { }
 
   ngOnInit() {
